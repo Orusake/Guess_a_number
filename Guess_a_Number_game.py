0494badb8ae8a7cd96062d9e5b0e69159e1d8f25
Orusake <thuhang.chau@gmail.com>
2020-07-14 16:09:58 +0200

    Update Guess_a_Number_game.py
    Add except ValueError

--- Guess_a_Number_game.py
+++ Guess_a_Number_game.py
@@ -3,7 +3,7 @@ import random
 guessesTaken = 0
 
 def nb_of_guesses(guess):
-    print("Now you have", 5 - guessesTaken , "guesses remanining.")
+    print("Now you have", 6 - guessesTaken , "guesses remaining.")
 
 print("Hello! What's your name?")
 myName = input()
@@ -11,27 +11,36 @@ myName = input()
 number = random.randint(1, 20)
 print("Well," + myName + ", I am thinking of a number between 1 and 20.")
 
-for guessesTaken in range(6):  #do this 6 times
-    print("Take a guess.")
-    guess = input()
-    guess = int(guess)
-    
-    
-    if guess < number:
-        print("Your guess is too low.")
-    
-    if guess > number:
-        print("Your guess it too high.")
-    
-    nb_of_guesses(guess)
-    
+while guessesTaken < 6:   
+    for guessesTaken in range(6):  #do this 6 times
+        print("Take a guess.")
+             
+        try: 
+            guess = int(input())
+            guessesTaken = guessesTaken + 1
+
+        except ValueError:
+            print("Don't try to fool me, you fool! Try again.")
+            continue
+
+        if guess < number:
+            print("Your guess is too low.")
+
+        if guess > number:
+            print("Your guess it too high.")
+
+        nb_of_guesses(guess)
+
+        if guess == number:
+            break
+                       
+            
     if guess == number:
+        guessesTaken = str(guessesTaken)
+        print("Bravo, " + myName + "! You guessed my number in " + guessesTaken + " guesses!")
         break
-        
-if guess == number:
-    guessesTaken = str(guessesTaken + 1)
-    print("Bravo, " + myName + "! You guessed my number in " + guessesTaken + " guesses!")
-    
-if guess != number:
-    number = str(number)
-    print("How bad, "+ myName + ". The number I was thinking of was " + number +".")
+
+    else:
+    # if guess != number:
+        number = str(number)
+        print("How bad, "+ myName + ". The number I was thinking of was " + number +".")
